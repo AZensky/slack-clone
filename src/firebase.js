707cdd4fff914d70c0131592574ced23a14a3cd9
Alex Zelinsky <alexzelinsky124@gmail.com>
2022-04-01 16:47:55 -0700

    header complete

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXnOTJLf1qSj8Dh-aPfGpsbuV3QV4mPR4",
+  authDomain: "slack-clone-6c928.firebaseapp.com",
+  projectId: "slack-clone-6c928",
+  storageBucket: "slack-clone-6c928.appspot.com",
+  messagingSenderId: "1035760747190",
+  appId: "1:1035760747190:web:97d8caaf2db4297b269c17",
+};
