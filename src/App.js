ed50bb6fe7de99b301013cbbcf5027b713eba522
Alex Zelinsky <alexzelinsky124@gmail.com>
2022-04-01 17:36:19 -0700

    sidebar draft

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import "./App.css";
 import { Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Header />} />
-      </Routes>
+      <Header />
+
+      <AppBody>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+        </Routes>
+      </AppBody>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
